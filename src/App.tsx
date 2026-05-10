56deab69fbcfd79a0ad8708c0043bc3ada666724
Ahmad Alsaleh <aalsalh474@gmail.com>
2026-05-10 15:30:28 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import NotFoundPage from "./components/NotFoundPage";
+
 export default function App() {
-  return <>a</>;
+  return (
+    <Routes>
+      <Route path="/" Component={Layout}>
+        <Route index />
+        <Route path="search:name" />
+        <Route path="ad:id" />
+        <Route path="user:id" />
+        <Route path="me">
+          <Route index />
+          <Route path="edit" />
+          <Route path="post-ad" />
+          <Route path="edit-ad:id" />
+        </Route>
+      </Route>
+      <Route path="login" />
+      <Route path="register" />
+      <Route path="*" Component={NotFoundPage} />
+    </Routes>
+  );
 }
