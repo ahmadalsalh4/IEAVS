0b3cf2a8403fb915af314763e4d5abbf34884de1
Ahmad Alsaleh <aalsalh474@gmail.com>
2026-05-24 03:59:14 +0300

    a

--- src/features/public/home/HomePage.tsx
+++ src/features/public/home/HomePage.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router";
 export default function HomePage() {
   return (
     <div>
-      <h1></h1>
+      <div className="mt-3 flex justify-center gap-2">
+        <input type="text" />
+        <Link to={""}>ara</Link>
+      </div>
+      <div className="mt-3 flex justify-center gap-2">
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+        <Link to={""}>ara</Link>
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
